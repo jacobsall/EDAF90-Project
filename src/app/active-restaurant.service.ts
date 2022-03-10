8287a774369fb58aac05be0eddc5ea2b32e1a0b4
ax6160sv-s <ax6160sv-s@student.lu.se>
2022-03-10 21:42:57 +0100

    Add cart functionality to activeRestaurantService

--- src/app/active-restaurant.service.ts
+++ src/app/active-restaurant.service.ts
@@ -6,14 +6,31 @@ import { BehaviorSubject, Observable, Subject } from 'rxjs';
 })
 export class ActiveRestaurantService {
 
-  private subject: Subject<any> = new BehaviorSubject<any>(undefined);
+  private restaurantSubject: Subject<any> = new BehaviorSubject<any>(undefined);
+
+  private cartSubject: BehaviorSubject<any> = new BehaviorSubject<any>(undefined);
 
   getActiveRestaurant(): Observable<any> {
-    return this.subject.asObservable();
-  } 
+    return this.restaurantSubject.asObservable();
+  }
 
   setActiveRestaurant(id: string) {
-    this.subject.next({activeId: id});
+    this.restaurantSubject.next({activeId: id});
+  }
+
+  getCart(): Observable<any> {
+    return this.cartSubject.asObservable();
+  }
+
+  addToCart(toAdd: string) {
+    const currentValue = this.cartSubject.value || {};
+    if (currentValue[toAdd]) {
+      currentValue[toAdd]++;
+    } else {
+      currentValue[toAdd] = 1;
+    }
+
+    this.cartSubject.next(currentValue);
   }
 
   constructor() {}
--- src/app/view-menu/view-menu.component.ts
+++ src/app/view-menu/view-menu.component.ts
@@ -1,36 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { MOCK_DATA } from '../mock-restaurant';
+import { ActiveRestaurantService } from '../active-restaurant.service';
 
 @Component({
   selector: 'app-view-menu',
   templateUrl: './view-menu.component.html',
   styleUrls: ['./view-menu.component.css']
 })
-export class ViewMenuComponent implements OnInit {
+export class ViewMenuComponent implements OnInit, OnDestroy {
 
   private id = history.state.id || 0;
   menu = MOCK_DATA[this.id].menu;
   pizzas = Object.keys(this.menu);
-  private done = false;
+  private cart: any;
+  private cartSub: any;
 
   public ingredientString(pizza: string) {
     return this.menu[pizza].ingredients.reduce((tot,curr) => tot + ", " + curr);
   }
 
-  public onClick() {
-    console.log(this.menu);
-    this.done = true;
+  public addPizza(name: string) {
+    this.activeService.addToCart(name);
+    console.log(this.cart);
   }
 
   public hasPicked() {
-    return this.done;
+    return this.cart !== undefined;
   }
 
-  constructor(private router: Router) {}
+  constructor(private activeService: ActiveRestaurantService) {}
+
+  private getCart() {
+    return this.activeService.getCart()
+      .subscribe(item => {
+        this.cart = item;
+      });
+  }
 
   ngOnInit(): void {
-    console.log("I viewMeni");
+    this.cartSub = this.getCart();
+  }
+
+  ngOnDestroy(): void {
+    this.cartSub.unsubscribe();
   }
 
 }
