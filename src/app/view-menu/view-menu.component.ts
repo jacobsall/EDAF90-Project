46f4af334d6241cd8eb027afaa365deba23f2fc7
linusabrink <li1304ab-s@student.lu.se>
2022-03-10 22:58:28 +0100

    Uses correct data in viewmenu

--- src/app/view-menu/view-menu.component.ts
+++ src/app/view-menu/view-menu.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { MOCK_DATA } from '../mock-restaurant';
 import { GlobalDataService } from '../global-data.service';
-
+import { RestaurantsService } from '../restaurants.service';
 @Component({
   selector: 'app-view-menu',
   templateUrl: './view-menu.component.html',
@@ -11,13 +11,15 @@ import { GlobalDataService } from '../global-data.service';
 export class ViewMenuComponent implements OnInit, OnDestroy {
 
   private id = history.state.id || 0;
-  menu = MOCK_DATA[this.id].menu;
-  pizzas = Object.keys(this.menu);
+  public menu: any;
+  public pizzas: any;
   private cart: any;
   private cartSub: any;
+  private active: any;
+  private restaurants: any;
 
   public ingredientString(pizza: string) {
-    return this.menu[pizza].ingredients.reduce((tot,curr) => tot + ", " + curr);
+    return this.menu[pizza].ingredients.reduce((tot: string,curr:string) => tot + ", " + curr);
   }
 
   public addPizza(name: string) {
@@ -29,7 +31,10 @@ export class ViewMenuComponent implements OnInit, OnDestroy {
     return this.cart !== undefined;
   }
 
-  constructor(private dataService: GlobalDataService) {}
+  constructor(
+    private dataService: GlobalDataService,
+    private restaurantsService: RestaurantsService,
+    ) {}
 
   private getCart() {
     return this.dataService.getCart()
@@ -40,10 +45,25 @@ export class ViewMenuComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.cartSub = this.getCart();
+    this.getActiveRestaurant();
   }
 
   ngOnDestroy(): void {
     this.cartSub.unsubscribe();
   }
 
+  private getActiveRestaurant(): void {
+    this.dataService.getActiveRestaurant()
+      .subscribe(item => {
+        this.active = item;
+        this.getMenu();
+    });
+  }
+  private getMenu(): void {
+    this.restaurantsService.getRestaurants()
+      .subscribe(items => {this.restaurants = items;
+      this.menu = this.restaurants.find((a: any) => a.id === this.active?.activeId)?.menu;
+      this.pizzas = this.menu && Object.keys(this.menu);
+    });
+  }
 }
