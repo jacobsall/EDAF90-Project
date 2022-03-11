4faf02a9d900be4614dd64b8c1c544506c5158b0
linusabrink <li1304ab-s@student.lu.se>
2022-03-11 10:05:44 +0100

    view-cart shows item but not price or amount

--- src/app/view-cart/view-cart.component.ts
+++ src/app/view-cart/view-cart.component.ts
@@ -1,16 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { GlobalDataService } from '../global-data.service';
 
+
 @Component({
   selector: 'app-view-cart',
   templateUrl: './view-cart.component.html',
   styleUrls: ['./view-cart.component.css']
 })
 export class ViewCartComponent implements OnInit {
+  public viewCart: any;
+  private cart: any;
+
 
   constructor(private globalService: GlobalDataService) { }
 
   ngOnInit(): void {
+    this.getCart();
   }
+
+  private getCart() {
+    return this.globalService.getCart()
+      .subscribe(item => {
+        this.cart = item;
+        this.viewCart = this.cart && Object.keys(this.cart);
+      });
+  }
+
+  /*cartString(item: string) {
+    return this.cart[item].reduce((tot: string,curr:string) => tot + ", " + curr);
+  }*/
 
 }
